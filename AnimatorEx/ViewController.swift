2edf41a1822eda8e961c3e4e3d50a5e122bcfce2
Emily Kolar <emkolardev@gmail.com>
2018-12-08 13:01:06 -0600

    animations

--- AnimatorEx/ViewController.swift
+++ AnimatorEx/ViewController.swift
@@ -8,18 +8,98 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+private enum FocusState {
+    case focused
+    case unfocused
+}
 
+extension FocusState {
+    var opposite: FocusState {
+        switch self {
+            case .focused: return .unfocused
+            case .unfocused: return .focused
+        }
+    }
+}
+
+class ViewController: UIViewController {
+    @IBOutlet weak var sourceView: UIView!
+    @IBOutlet weak var destView: UIView!
+    @IBOutlet weak var transitionView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        destView.layer.cornerRadius = 8
+        destView.alpha = 0
+        sourceView.alpha = 0
+        sourceView.layer.zPosition = 1
+        destView.layer.zPosition = 2
+        transitionView.layer.zPosition = 3
+        transitionView.addGestureRecognizer(tapRecognizer)
+        transitionView.morphRectTo(sourceView)
     }
+    
+    private var currentState: FocusState = .unfocused
+    
+    private lazy var tapRecognizer: UITapGestureRecognizer = {
+        let recognizer = UITapGestureRecognizer()
+        recognizer.addTarget(self, action: #selector(viewTapped(recognizer:)))
+        return recognizer
+    }()
+    
+    @objc private func viewTapped(recognizer: UITapGestureRecognizer) {
+        print("tapped")
+        let state = currentState.opposite
+        let transitionAnimator = UIViewPropertyAnimator(duration: 1, dampingRatio: 1, animations: {
+            switch state {
+            case .focused:
+                self.transitionView.morphRectTo(self.destView)
+                self.currentState = state
+            case .unfocused:
+                self.transitionView.morphRectTo(self.destView)
+                self.currentState = state
+            }
+            self.view.layoutIfNeeded()
+        })
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        transitionAnimator.startAnimation()
     }
+    
+}
 
+public extension UIView {
+    func transformTo(_ view: UIView) {
+        transform = transformRect(from: frame, to: view.frame)
+    }
+    
+    func morphRectTo(_ view: UIView, scale: CGFloat = 1) {
+        guard let convertedFrame = superview?.convert(view.frame, from: view.superview) else {
+            return
+        }
 
+        backgroundColor = view.backgroundColor
+
+        layer.cornerRadius = view.layer.cornerRadius * scale
+        frame = convertedFrame * scale
+    }
+    
+    private func transformRect(from source: CGRect, to destination: CGRect) -> CGAffineTransform {
+        return CGAffineTransform.identity
+            .translatedBy(x: destination.midX - source.midX,
+                          y: destination.midY - source.midY)
+            .scaledBy(x: destination.width / source.width,
+                      y: destination.height / source.height)
+    }
+}
+
+fileprivate extension CGRect {
+    
+    static func *(lhs: CGRect, rhs: CGFloat) -> CGRect {
+        let scaledWidth = lhs.width * rhs
+        let scaledHeight = lhs.height * rhs
+        let x = lhs.origin.x + (lhs.width - scaledWidth) / 2
+        let y = lhs.origin.y + (lhs.height - scaledHeight) / 2
+        return CGRect(x: x, y: y, width: scaledWidth, height: scaledHeight)
+    }
 }
 
